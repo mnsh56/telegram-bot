3a04814ba0a664571d843682d37a90e427072d00
mnsh56 <m_n_sh_77@yahoo.com>
2025-04-26 21:29:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ def start_bot():
 threading.Thread(target=start_bot, daemon=True).start()
 
 # اجرای Flask برای Render
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot is running!"
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
