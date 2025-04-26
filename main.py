d0506e24c81c5a39e7a9387e41e2dafb1ae0ea19
mnsh56 <m_n_sh_77@yahoo.com>
2025-04-26 21:18:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ MY_ID = 288677626
 
 bot = telebot.TeleBot(API_TOKEN)
 
+# قبل از شروع polling، Webhook قبلی رو حذف کن
+bot.remove_webhook()
+
 # هندلر برای تمام پیام‌ها (متن، عکس، ویدیو، ویس و...)
 @bot.message_handler(content_types=['text', 'photo', 'video', 'document', 'audio', 'voice', 'sticker'])
 def forward_all(message):
@@ -20,11 +23,11 @@ def forward_all(message):
 threading.Thread(target=bot.polling, daemon=True).start()
 
 # اجرای Flask برای Render
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/')
 def home():
     return "Bot is running!"
 
 port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0", port=port)
