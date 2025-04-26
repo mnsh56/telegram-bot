dc007d7eb8eee47c294d912aba8adcb9c1d708c2
mnsh56 <m_n_sh_77@yahoo.com>
2025-04-26 21:31:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ app = Flask(__name__)
 def home():
     return "Bot is running!"
 
-if __name__ == '__main__':
+if name == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
